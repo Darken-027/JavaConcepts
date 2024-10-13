07061ae3fcdbd201c0d9112249c5379867f6e079
Darken-027 <torut1638@gmail.com>
2024-10-13 17:41:55 -0500

    Arithmetic Operators in Java

---
+++ src/math/Operators.java
@@ -0,0 +1,19 @@
+package math;
+
+public class Operators {
+    public static void main(String[] args) {
+        int num1 = 7;
+        int num2 = 5;
+
+        // incremt num1 in 3
+        //Decrement and increment
+        //num1 += 3;
+        //num2++;
+        //num2--;
+        // --num1;
+        // ++num2;
+
+        int result = num1 / num2;
+        System.out.println(result);
+    }
+}
