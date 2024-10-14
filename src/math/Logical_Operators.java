142d257b73cf33744d8bec1a8d3c35385ca470b6
Darken-027 <torut1638@gmail.com>
2024-10-13 20:02:51 -0500

    Logical Operators

---
+++ src/math/Logical_Operators.java
@@ -0,0 +1,13 @@
+package math;
+
+public class Logical_Operators {
+    public static void main(String[] args) {
+        int x = 7;
+        int y = 8;
+        int a = 4;
+        int b = 1;
+
+        boolean result = x > y && a < b;
+        System.out.println(result);
+    }
+}
