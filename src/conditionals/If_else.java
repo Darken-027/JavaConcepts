0da1050f835176004677b4503d0de13343cc8b43
Darken-027 <torut1638@gmail.com>
2024-10-14 15:01:16 -0500

    if else

---
+++ src/conditionals/If_else.java
@@ -0,0 +1,14 @@
+package conditionals;
+
+public class If_else {
+    public static void main(String[] args) {
+
+        int x = 18;
+
+        if(x > 10 && x <= 20){
+            System.out.println("Hello");
+        } else{
+            System.out.println("Bye");
+        }
+    }
+}
