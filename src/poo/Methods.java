04c2a311bd515e6689fe59dc3cb5b9f30203e725
Darken-027 <torut1638@gmail.com>
2024-10-28 20:06:24 -0500

    Methods

--- src/poo/Methods.java
+++ src/poo/Methods.java
@@ -1,7 +1,26 @@
 package poo;
 
+
+
 public class Methods {
+
+    public void playMusic(){
+        System.out.println("Music playing...");
+    }
+
+    public String getMeApen(int cost){
+        return "pen";
+    }
+
+
     public static void main(String[] args) {
-        
+
+
+        Methods music = new Methods();
+        music.playMusic();
+        music.getMeApen(10);
+
+
+
     }
 }
