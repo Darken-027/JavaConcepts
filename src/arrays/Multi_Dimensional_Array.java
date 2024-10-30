031984f902b3f66ece8f80d241c5243ed052e427
Darken-027 <torut1638@gmail.com>
2024-10-29 21:47:47 -0500

    Multi dimensional array

---
+++ src/arrays/Multi_Dimensional_Array.java
@@ -0,0 +1,16 @@
+package arrays;
+
+public class Multi_Dimensional_Array {
+    public static void main(String[] args) {
+        
+        
+        int nums[][] = new int[3][4];
+
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j<4; j++){
+                System.out.println(nums[i][j] + "");
+            }
+            System.out.println();
+        }
+    }
+}
