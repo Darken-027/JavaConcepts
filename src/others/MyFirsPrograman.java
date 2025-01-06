45723abadc143bf0905122ef82681207c907318a
Ahstell <torut1638@gmail.com>
2025-01-05 19:47:28 -0600

    Implements, The class MyFirstProgram and Variable, the variable class has the data types of java, has the data types in a comment block

--- src/others/MyFirsPrograman.java
+++ src/others/MyFirsPrograman.java
@@ -1,3 +1,5 @@
+package others;
+
 public class MyFirsPrograman {
     public static void main(String[] args) {
         System.out.println("I love Pizza");
---
+++ src/others/Variable.java
@@ -0,0 +1,31 @@
+package others;
+
+public class Variable {
+    public static void main(String[] args) {
+        //Variables
+        /**
+        * A placeholder for a value that behaves as the value
+         * it contains
+         *      forexample
+         *      x = 123
+         *      y = "hello"
+         *      z = true
+         *
+         *      DATA TYPES
+         *
+         *    # boolean 1bit    primitive   true or false
+         *      byte    1byte   primitive   -128 to 127
+         *      short   2bytes  primitive   -32,768 to 32,767
+         *   #  int     4bytes  primitive   -2 billion to 2 billion
+         *      long    8bytes  primitive   -9 quintillon to 9 quintillon
+         *
+         *
+         *      float   4bytes primitive    fractional number up to 6-7 digits ex. 3.141592
+         *   #  double  8bytes primitive    fractional number up to 15 digits ex. 3.141592653589793
+         *
+         *   #  char    2bytes primitive    single character/latter/ASCII value ex. 'f'
+         *   #  String  varies referente    a sequence of characters ex. "Hello World"
+         *
+        * **/
+    }
+}
