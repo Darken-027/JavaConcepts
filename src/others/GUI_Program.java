873f78300fe585ff0ba57bdbc84d408f470ff542
Ahstell <torut1638@gmail.com>
2025-01-08 19:40:14 -0600

    GUI Program

--- src/others/GUI_Program.java
+++ src/others/GUI_Program.java
@@ -8,5 +8,14 @@ public class GUI_Program {
 
         String name = JOptionPane.showInputDialog("Enter your name");
         JOptionPane.showMessageDialog(null, "Hello " + name);
+
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age. "));
+        JOptionPane.showMessageDialog(null, "You are " + age + "years old");
+
+        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height. "));
+        JOptionPane.showMessageDialog(null, "You are " + height + "cm tall");
+
+
+
     }
 }
