53b00d9818007e1e9033f2f299edece0d2a8e9d7
Ahstell <torut1638@gmail.com>
2025-01-15 20:21:52 -0600

    comments and basic structure

--- src/others/MyFirstClass.java
+++ src/others/MyFirstClass.java
@@ -1,5 +1,14 @@
 package others;
 
 public class MyFirstClass {
-    
+    public static void main(String[] args) {
+        //Comments
+
+        /*
+         * Block comments
+         * 
+         */
+        System.out.println("Hello World");
+        System.out.println("My name is Ahstell");
+    }
 }
