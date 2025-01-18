cfa2460e4421d5b64c330f76e5f0f1c75e91dd8a
Ahstell <torut1638@gmail.com>
2025-01-17 21:40:22 -0600

    literals

--- src/others/Literal.java
+++ src/others/Literal.java
@@ -1,5 +1,19 @@
 package others;
 
 public class Literal {
+    public static void main(String[] args) {
+        //Literal
+        byte variableByte = 12;
+        short variableShort = 12;
+        int variableInt = 12;
+        long variableLong = 12l;
 
+        //decimal literal
+        float variableFloat = 12.2F;
+        double variableDouble = 12.2;
+
+        //Character and string literals
+        String pais = "Espau00F1a";
+
+    }
 }
