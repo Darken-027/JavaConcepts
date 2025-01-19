31db45af21fbc290d3e048faf6b70615f8f3e732
Ahstell <torut1638@gmail.com>
2025-01-19 15:29:42 -0600

    Add floating point variables

--- src/others/Variable.java
+++ src/others/Variable.java
@@ -4,71 +4,94 @@ import java.util.Scanner;
 
 public class Variable {
     public static void main(String[] args) {
-        //Variables
+        // Variables
         /**
          * A placeholder for a value that behaves as the value
          * it contains
-         *      forexample
-         *      x = 123
-         *      y = "hello"
-         *      z = true
+         * forexample
+         * x = 123
+         * y = "hello"
+         * z = true
          *
-         *      DATA TYPES
+         * DATA TYPES
          *
-         *    # boolean 1bit    primitive   true or false
-         *      byte    1byte   primitive   -128 to 127
-         *      short   2bytes  primitive   -32,768 to 32,767
-         *   #  int     4bytes  primitive   -2 billion to 2 billion
-         *      long    8bytes  primitive   -9 quintillon to 9 quintillon
-         *
-         *
-         *      float   4bytes primitive    fractional number up to 6-7 digits ex. 3.141592
-         *   #  double  8bytes primitive    fractional number up to 15 digits ex. 3.141592653589793
-         *
-         *   #  char    2bytes primitive    single character/latter/ASCII value ex. 'f'
-         *   #  String  varies referente    a sequence of characters ex. "Hello World"
+         * # boolean 1bit primitive true or false
+         * byte 1byte primitive -128 to 127
+         * short 2bytes primitive -32,768 to 32,767
+         * # int 4bytes primitive -2 billion to 2 billion
+         * long 8bytes primitive -9 quintillon to 9 quintillon
          *
          *
-         *      How to create a variable
+         * float 4bytes primitive fractional number up to 6-7 digits ex. 3.141592
+         * # double 8bytes primitive fractional number up to 15 digits ex.
+         * 3.141592653589793
          *
-         *          int x;  declaraction
-         *          x = 123;    assignment
-         *          int x = 123;    initialization
-         *          long x = 122444343555342424;
-         *
-         *
-         *
-         *          When we need to store a larger number, we use the long data type, and we do the conversion with the capital letter L,
-         *          placed after the number, for example, long x = 122444343555342424L;
+         * # char 2bytes primitive single character/latter/ASCII value ex. 'f'
+         * # String varies referente a sequence of characters ex. "Hello World"
          *
          *
-         *          input in java
+         * How to create a variable
          *
-         *          impor java.itul.Scanner;
-         *          Scanner scanner = new Scanner(System.in);
+         * int x; declaraction
+         * x = 123; assignment
+         * int x = 123; initialization
+         * long x = 122444343555342424;
+         *
+         *
+         *
+         * When we need to store a larger number, we use the long data type, and we do
+         * the conversion with the capital letter L,
+         * placed after the number, for example, long x = 122444343555342424L;
+         *
+         *
+         * input in java
+         *
+         * impor java.itul.Scanner;
+         * Scanner scanner = new Scanner(System.in);
          *
          **/
 
-//        String name = "";
-//        int age = 0;
-//
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.print("what is your name: ");
-//        name = scanner.nextLine();
-//        System.out.print("how old are you ? ");
-//        age = scanner.nextInt();
-//
-//
-//
-//        System.out.println("hello " + name);
-//        System.out.print("Your age is.. " + age + " year old");
+        // String name = "";
+        // int age = 0;
+        //
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.print("what is your name: ");
+        // name = scanner.nextLine();
+        // System.out.print("how old are you ? ");
+        // age = scanner.nextInt();
+        //
+        //
+        //
+        // System.out.println("hello " + name);
+        // System.out.print("Your age is.. " + age + " year old");
 
-
-        //Expression
+        // Expression
         double friends = 10;
 
         friends--;
 
         System.out.println(friends);
+
+        // Iteger type variables
+        byte bytel;
+        short shortl;
+        int intl;
+        long longl;
+        bytel = 1;
+        shortl = 100;
+        intl = 10000;
+        longl = 100000000;
+        System.out.println("byte1 = " + bytel);
+        System.out.println("shortl = " + shortl);
+        System.out.println("int = " + intl);
+        System.out.println("ongl = " + longl);
+
+        // floating point variables
+        float floatl;
+        double doublel;
+        floatl = 1.11111111111F;
+        doublel = 1.1111111111111E+9D;
+        System.out.println("f1oatl = " + floatl);
+        System.out.println("doub1el = " + doublel);
     }
 }
