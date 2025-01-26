f14b6b8455e0f9795fdec8cc5b370f9d7534d44f
Ahstell <torut1638@gmail.com>
2025-01-25 23:11:39 -0600

    unidimensional arrays

--- src/arrays/UnidimensionalArrays.java
+++ src/arrays/UnidimensionalArrays.java
@@ -2,6 +2,15 @@ package arrays;
 
 public class UnidimensionalArrays {
     public static void main(String[] args) {
-        
+        //Declare an array of doubles 
+        double accounts[];
+        accounts = new double[100];
+        accounts[3] = 1340.0;
+        System.out.println("Account 3 must: $" + accounts[3]);
+
+        //Other form[[]
+        double accounte[] = new double [1001];
+        accounte[4] = 122.45;
+        System.out.println("Account 4 must: $" + accounte[3]);
     }
 }
