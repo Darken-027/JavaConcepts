515ede7404450f23c18b774410991fd1cd407915
Ahstell <torut1638@gmail.com>
2025-01-28 20:35:40 -0600

    change line

--- src/arrays/Exercise_number_1.java
+++ src/arrays/Exercise_number_1.java
@@ -2,6 +2,7 @@ package arrays;
 
 public class Exercise_number_1 {
     public static void main(String[] args) {
+        // array containing the numbers from 1 to 100
         int numbers[] = new int[100];
 
         for(int i = 0; i < 100; i++){
---
+++ src/arrays/Exercise_number_2.java
@@ -0,0 +1,7 @@
+package arrays;
+
+public class Exercise_number_2 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
