84b53aee5900e0f6f7f73428b117a828b30ddb28
Ahstell <torut1638@gmail.com>
2025-01-29 21:16:23 -0600

    hello wordl

--- src/arrays/Exercise_number_2.java
+++ src/arrays/Exercise_number_2.java
@@ -2,6 +2,6 @@ package arrays;
 
 public class Exercise_number_2 {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello world");
     }
 }
