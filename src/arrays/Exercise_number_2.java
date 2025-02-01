2a35d4db726b4d5b2896cf6be78c7a3af57db583
Ahstell <torut1638@gmail.com>
2025-01-31 22:01:10 -0600

    type of variables

--- src/arrays/Exercise_number_2.java
+++ src/arrays/Exercise_number_2.java
@@ -2,6 +2,9 @@ package arrays;
 
 public class Exercise_number_2 {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        int x, y, z;
+        x = y = z = 50;
+        System.out.println(x + y + z);
+        
     }
 }
--- src/others/Variable.java
+++ src/others/Variable.java
@@ -137,5 +137,18 @@ public class Variable {
         int intC = intA * intB;
         System.out.println(intC = -+intC);
 
+        // Student data
+        String studentName = "John Doe";
+        int studentID = 15;
+        int studentAge = 23;
+        float studentFee = 75.25f;
+        char studentGrade = 'B';
+
+        // Print variables
+        System.out.println("Student name: " + studentName);
+        System.out.println("Student id: " + studentID);
+        System.out.println("Student age: " + studentAge);
+        System.out.println("Student fee: " + studentFee);
+        System.out.println("Student grade: " + studentGrade);
     }
 }
