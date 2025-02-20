09dc7947653003ca5e9d3d067d0a963adcb50554
Ahstell <torut1638@gmail.com>
2025-02-19 23:06:36 -0500

    prueva

--- src/others/Variable.java
+++ src/others/Variable.java
@@ -150,5 +150,29 @@ public class Variable {
         System.out.println("Student age: " + studentAge);
         System.out.println("Student fee: " + studentFee);
         System.out.println("Student grade: " + studentGrade);
+
+        // Create integer variables
+        int length = 4;
+        int width = 6;
+        int area;
+
+        // Calculate the area of a rectangle
+        area = length * width;
+        
+        // Print variables
+        System.out.println("Length is: " + length);
+        System.out.println("Width is: " + width);
+        System.out.println("Area of the rectangle is: " + area);
+
+        // Create variables of different data types
+        int items = 50;
+        float costPerItem = 9.99f;
+        float totalCost = items * costPerItem;
+        char currency = '$';
+
+        // Print variables
+        System.out.println("Number of items: " + items);
+        System.out.println("Cost per item: " + costPerItem + currency);
+        System.out.println("Total cost = " + totalCost + currency);
     }
 }
